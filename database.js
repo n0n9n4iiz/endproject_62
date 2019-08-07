6596179f3a04b323b47697d2b7dc340f19adbe3e
krittayot <nai-iiz@hotmail.co.th>
2019-08-07 18:48:00 +0700

    เริ่มต้นต่อดาต้าเบสจาก heroku แบบธรรมดา

---
+++ database.js
@@ -0,0 +1,26 @@
+const pgp = require('pg-promise')(); 
+var db = pgp('postgres://ejhivrfxrvzsam:e6ea27848d6af9115aa2c68040e674502dbfb9d288840cac04cbda3e90607967@ec2-107-20-167-241.compute-1.amazonaws.com:5432/d80ue59kfg9h7u?ssl=true');
+
+function getAllPerson(req, res) {
+   // console.log("req = "+req.params.idp);
+    
+    db.any('select * from persons').then(function (data) {
+        res.status(200).json({
+         status: 'success',
+         data: data,
+          message: 'Retrieved ALL products'
+        });
+    }).catch(function (error) {
+        console.log(error);
+        res.status(500).json({
+            status: 'failed',
+            data: data,
+            message: 'Failed To Retrieved ALL products'
+        });
+    })
+}
+
+
+    module.exports = { 
+        getAllPerson
+    }
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var db = require('./database'); 
+var bodyParser = require('body-parser');
+var cors = require('cors');
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+extended: true
+}));
+
+app.get('/', function (req, res) {
+    res.send('Express is running');
+});
+
+app.get('/allperson',db.getAllPerson);
+
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+console.log('App is running on http://localhost:' + port);
+});    
