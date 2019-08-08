e8aa8de5b12df6dcf56c8823855f335cde199c08
krittayot <nai-iiz@hotmail.co.th>
2019-08-09 03:30:33 +0700

    ข้อมูลจำลองใช้ได้สำเร็จ

--- database.js
+++ database.js
@@ -2,8 +2,6 @@ const pgp = require('pg-promise')();
 var db = pgp('postgres://ejhivrfxrvzsam:e6ea27848d6af9115aa2c68040e674502dbfb9d288840cac04cbda3e90607967@ec2-107-20-167-241.compute-1.amazonaws.com:5432/d80ue59kfg9h7u?ssl=true');
 
 function getAllPerson(req, res) {
-    // console.log("req = "+req.params.idp);
-
     db.any('select * from persons').then(function (data) {
         res.status(200).json({
             status: 'success',
@@ -22,7 +20,6 @@ function getAllPerson(req, res) {
 
 function getPerson(req, res) {
     // console.log("req = "+req.params.idp);
-
     db.any('select * from persons where personid = ' + req.params.id).then(function (data) {
         res.status(200).json({
             status: 'success',
